868ddb7554c487cb18d9895c72e5dd4ba27c4d1e
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-27 12:11:28 +0500

    auth

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
-const cookieParser = require('cookie-parser');
 const express = require('express');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
 const app = express();
 
 app.set('view engine', 'ejs');
+
+// Set views folder explicitly (recommended)
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-
 app.get('/', (req, res) => {
-    res.send('Well come')
-})
+    res.render('index');  // ✅ Correct
+});
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
