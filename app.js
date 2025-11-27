8fb4a48189a55a67d0fa9d2ce099af237ca893db
codeby-ibrahim <muhammadibrahimkhan5557@gmail.com>
2025-11-27 11:45:43 +0500

    commit

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
 
 app.get('/', (req, res) => {
     res.send('Well come')
